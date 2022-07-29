72dd2f59b3540630784c6afdc21419b0f22e2a43
hardi-weybee <hardi@webtheuniverse.com>
2022-07-29 18:39:39 +0530

    object

--- program/Program.cs
+++ program/Program.cs
@@ -6,6 +6,21 @@ using System.Threading.Tasks;
 
 namespace program
 {
+    public class Student
+    {
+        public string name;
+        public int age;
+        public void Insert(string str, int number)
+        {
+            name = str;
+            age = number;
+        }
+
+        public void Print()
+        {
+            Console.WriteLine(name + " " + age);
+        }
+    }
     class Program
     {
         void FirstFunction()
@@ -178,18 +193,18 @@ namespace program
 
             //// functions
             //// 1. function with no parameter and return type
+
             //Program first = new Program();
+
             //first.FirstFunction();
 
 
             //// 2. function with parameter but no return type
-            //Program second = new Program();
-            //second.SecondFunction("Hardi");
+            //first.SecondFunction("Hardi");
 
 
             //// 3. function with parameter and return type
-            //Program third = new Program();
-            //string mes = third.ThirdFunction("Hardi");
+            //string mes = first.ThirdFunction("Hardi");
             //Console.Write("\nHey " + mes);     // parameter passing
             //Console.ReadLine();
 
@@ -197,9 +212,8 @@ namespace program
             //// 4. call by value in function 
             //int num = 100;
             //Console.Write("----------------Call by value--------------------");
-            //Program forth = new Program();
             //Console.Write("\nValue before function call : " + num);
-            //forth.CallByValue(num);
+            //first.CallByValue(num);
             //Console.Write("\nValue after function call : " + num);
             //Console.ReadLine();
 
@@ -207,165 +221,179 @@ namespace program
             //// 5. call by reference in function
             //int number = 100;
             //Console.Write("\n------------------Call by reference-------------------");
-            //Program fifth = new Program();
             //Console.Write("\nValue before function call : " + number);
-            //fifth.CallByReference(ref number);
+            //first.CallByReference(ref number);
             //Console.Write("\nvalue after function call : " + number);
             //Console.ReadLine();
 
 
             //// 6. out parameter
             //int num1 = 10;
-            //Program sixth = new Program();
             //Console.Write("\n------------------Out parameter-------------------");
             //Console.Write("\nValue before function call : " + num1);
-            //sixth.OutParamater(out num1);
+            //first.OutParamater(out num1);
             //Console.Write("\nValue after function call : " + num1);
             //Console.ReadLine();
 
 
-            // Arrays
-            // 1. Single dimensional array
-            int[] array1 = { 10, 10, 10, 10, 10 };
+            //// Arrays
+            //// 1. Single dimensional array
+            //int[] array1 = { 10, 10, 10, 10, 10 };
 
-            //for(int i=0; i<array1.Length; i++)
+            ////for(int i=0; i<array1.Length; i++)
+            ////{
+            ////    Console.Write(array1[i]);
+            ////    Console.ReadLine();
+            ////}
+
+            //foreach (int i in array1)
             //{
-            //    Console.Write(array1[i]);
+            //    Console.Write(i + " ");
+            //    //Console.ReadLine();
+            //}
+
+
+            //// 2. Passing arrays to function
+            //int[] array2 = { 25, 10, 20, 15, 40, 50 };
+            //int[] array3 = { 12, 23, 44, 11, 54 };
+
+            //first.ArraytoFunction(array2);
+            //first.ArraytoFunction(array3);
+
+            //// Max and Min number from array
+            //int[] array4 = { 25, 10, 20, 15, 40, 50 };
+            //int[] array5 = { 12, 23, 44, 11, 54 };
+
+            //first.MaxNumber(array4);
+            //first.MaxNumber(array5);
+
+            //first.MinNumber(array4);
+            //first.MinNumber(array5);
+
+
+            //// 3. multi dimensional array
+            //int[,] array6 = { { 1, 2, 3 }, { 4, 5, 6 }, { 7, 8, 9 } };
+
+            //Console.WriteLine("\n-------------multidimensional array-------------");
+            //for (int i = 0; i < 3; i++)
+            //{
+            //    for (int j = 0; j < 3; j++)
+            //    {
+            //        Console.Write(array6[i, j] + " ");
+            //    }
             //    Console.ReadLine();
             //}
 
-            foreach (int i in array1)
+
+            //// 4. jagged array (array of array)
+            //int[][] array7 = new int[3][]
+            //{
+            //    new int[] { 38, 58, 29, 23, 38, 487, 239, 60, 437, 57},
+            //    new int[] { 25, 10, 20, 15, 40, 50 },
+            //    new int[] { 12, 23, 44, 11, 54 }
+            //};
+
+            //Console.WriteLine("\n----------jagged array------------");
+            //for (int i = 0; i < array7.Length; i++)
+            //{
+            //    for (int j = 0; j < array7[i].Length; j++)
+            //    {
+            //        Console.Write(array7[i][j] + " ");
+            //    }
+            //    Console.ReadLine();
+            //}
+
+
+            //// 5. params
+            //Console.WriteLine("\n-----------params-----------");
+            //first.ParamsFunction(10, 20, 30, 40, 50);
+
+            //first.ParamsObject("Hello Everyone", "Good to see you", 10000);
+
+
+            //// 6. array class and methods
+            //int[] array8 = { 47, 39, 1, 59, 89, 55 };
+            //int[] array9 = new int[6];
+
+            //Console.Write("\n------array methods---------");
+
+            //Console.Write("\nLength of array element is " + array8.Length);
+
+            //Array.Sort(array8);
+            //Console.Write("\nsorted array elements : ");
+            //first.print(array8);
+
+            //Console.Write("\nindex of element 30 in array : " + Array.IndexOf(array8, 30));
+
+            //Array.Reverse(array8);
+            //Console.Write("\narray is reversed : ");
+            //first.print(array8);
+
+            //Array.Copy(array8, array9, array8.Length);
+            //Console.Write("\narray is copied to second array having element : ");
+            //first.print(array9);
+            //Console.ReadLine();
+
+
+            //// 7. command line argument
+            //Console.Write("\n------------command line arguments------------");
+            //Console.Write("\nargument length : " + args.Length);
+            //Console.Write("\narguments are :");
+            //foreach(Object obj in args)
+            //{
+            //    Console.Write(obj);
+            //}
+            //Console.ReadLine();
+
+
+
+
+            //// Exercise pattern
+
+            //Console.Write("\n----------Print the pattern-----------");
+            //p:
+            //    Console.Write("\nEnter any number from 1 to 26 : ");
+            //    int pattern = Convert.ToInt32(Console.ReadLine());
+
+            //    if(pattern <= 0 || pattern > 26) 
+            //    {
+            //        goto p;
+            //    }
+
+            //for(int i=0; i<pattern; i++)
+            //{
+            //    //int x = 65;
+            //    for(int j=0; j<=i; j++)
+            //    {
+            //        Console.Write((char)(j + 65) + " ");
+            //        //x++;
+            //    }
+            //    for(int k=i-1; k>=0; k--)
+            //    {
+            //        Console.Write((char)(k + 65) + " ");               
+            //    }
+            //    Console.ReadLine();
+            //}
+
+
+            // Object and class 
+            Student info = new Student()
             {
-                Console.Write(i + " ");
-                //Console.ReadLine();
-            }
+                name = "Hardi",
+                age = 21
 
-
-            // 2. Passing arrays to function
-            int[] array2 = { 25, 10, 20, 15, 40, 50 };
-            int[] array3 = { 12, 23, 44, 11, 54 };
-
-            Program arr = new Program();
-            arr.ArraytoFunction(array2);
-            arr.ArraytoFunction(array3);
-
-            // Max and Min number from array
-            int[] array4 = { 25, 10, 20, 15, 40, 50 };
-            int[] array5 = { 12, 23, 44, 11, 54 };
-
-            Program num = new Program();
-            num.MaxNumber(array4);
-            num.MaxNumber(array5);
-
-            num.MinNumber(array4);
-            num.MinNumber(array5);
-
-
-            // 3. multi dimensional array
-            int[,] array6 = { { 1, 2, 3 }, { 4, 5, 6 }, { 7, 8, 9 } };
-
-            Console.WriteLine("\n-------------multidimensional array-------------");
-            for (int i = 0; i < 3; i++)
-            {
-                for (int j = 0; j < 3; j++)
-                {
-                    Console.Write(array6[i, j] + " ");
-                }
-                Console.ReadLine();
-            }
-
-
-            // 4. jagged array (array of array)
-            int[][] array7 = new int[3][]
-            {
-                new int[] { 38, 58, 29, 23, 38, 487, 239, 60, 437, 57},
-                new int[] { 25, 10, 20, 15, 40, 50 },
-                new int[] { 12, 23, 44, 11, 54 }
             };
-
-            Console.WriteLine("\n----------jagged array------------");
-            for (int i = 0; i < array7.Length; i++)
-            {
-                for (int j = 0; j < array7[i].Length; j++)
-                {
-                    Console.Write(array7[i][j] + " ");
-                }
-                Console.ReadLine();
-            }
-
-
-            // 5. params
-            Program param = new Program();
-            Console.WriteLine("\n-----------params-----------");
-            param.ParamsFunction(10, 20, 30, 40, 50);
-
-            param.ParamsObject("Hello Everyone", "Good to see you", 10000);
-
-
-            // 6. array class and methods
-            int[] array8 = { 47, 39, 1, 59, 89, 55 };
-            int[] array9 = new int[6];
-
-            Console.Write("\n------array methods---------");
-
-            Console.Write("\nLength of array element is " + array8.Length);
-
-            Array.Sort(array8);
-            Console.Write("\nsorted array elements : ");
-            Program a = new Program();
-            a.print(array8);
-
-            Console.Write("\nindex of element 30 in array : " + Array.IndexOf(array8, 30));
-
-            Array.Reverse(array8);
-            Console.Write("\narray is reversed : ");
-            a.print(array8);
-
-            Array.Copy(array8, array9, array8.Length);
-            Console.Write("\narray is copied to second array having element : ");
-            a.print(array9);
+            Console.Write(info.name + " " + info.age);
             Console.ReadLine();
 
-
-            // 7. command line argument
-            Console.Write("\n------------command line arguments------------");
-            Console.Write("\nargument length : " + args.Length);
-            Console.Write("\narguments are :");
-            foreach(Object obj in args)
-            {
-                Console.Write(obj);
-            }
+            Console.Write("\n------display data through method--------\n");
+            Student s1 = new Student();
+            Student s2 = new Student();
+            s1.Insert("Rishit", 21);
+            s2.Insert("Misari", 20);
+            s1.Print();
+            s2.Print();
             Console.ReadLine();
-
-
-
-
-            // Exercise pattern
-
-            Console.Write("\n----------Print the pattern-----------\n");
-            p:
-                Console.Write("\nEnter any number from 1 to 26 : ");
-                int pattern = Convert.ToInt32(Console.ReadLine());
-
-                if(pattern <= 0 || pattern > 26) 
-                {
-                    goto p;
-                }
-
-            for(int i=0; i<pattern; i++)
-            {
-                //int x = 65;
-                for(int j=0; j<=i; j++)
-                {
-                    Console.Write((char)(j + 65) + " ");
-                    //x++;
-                }
-                for(int k=i-1; k>=0; k--)
-                {
-                    Console.Write((char)(k + 65) + " ");               
-                }
-                Console.ReadLine();
-            }
         }
     }
 }
