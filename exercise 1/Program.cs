c267117dcc7b526376008da4162bef7766b82719
hardi-weybee <hardi@webtheuniverse.com>
2022-08-03 09:48:53 +0530

    Sorting Program

--- exercise 1/Program.cs
+++ exercise 1/Program.cs
@@ -10,10 +10,10 @@ namespace exercise_1
     {
         static void Main(string[] args)
         {
-            //// Exercise pattern
+            // Exercise pattern
             Console.Write("\n----------print the pattern-----------");
             pattern:
-                Console.Write("\nenter any number from 1 to 26 : ");
+                Console.Write("\nEnter any number from 1 to 26 : ");
                 int pattern = Convert.ToInt32(Console.ReadLine());
 
             if (pattern <= 0 || pattern > 26)
@@ -35,6 +35,37 @@ namespace exercise_1
                 }
                 Console.ReadLine();
             }
+
+
+
+            // sorting without using inbuilt method
+            Console.WriteLine("\n-----------Sorting in ascending order---------------");
+            int[] array = {74, 489, 78, 37, 2, 38, 55, 90, 68, 267};
+            int t = 0;
+            Console.WriteLine("\nArray before sorting");
+            foreach(int x in array)
+            {
+                Console.Write(x + " ");
+            }
+
+            for(int i=0; i<array.Length; i++)
+            {
+                for(int j=i+1; j<array.Length; j++)
+                {
+                    if(array[i] > array[j])
+                    {
+                        t = array[i];
+                        array[i] = array[j];
+                        array[j] = t;
+                    }
+                }
+            }     
+            Console.WriteLine("\n\nArray after sorting");
+            foreach(int y in array)
+            {
+                Console.Write(y + " ");
+            }
+            Console.ReadLine();
         }
     }
 }
